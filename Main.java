38180962ff9952d75f661bf4e22911dad636c24a
geraldjet <geraldpromchuakaew@gmail.com>
2021-09-22 17:00:26 -0700

    update starter

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type 1 to Sort Integer OR 2 to Sort String");
+        int n = in.nextInt();
+        if(n == 1)
+        sortInt();
+        else if(n == 2)
+        sortString();
+
+        }
+
+    //Sort Array Integer   
+    static void sortInt(){
+
+
+    }
+    //Sort Array String
+    static void sortString(){
+
+
+    }
+}
